64f4ae7ec8b293933b3d0eb8f7a608e6d56d715e
danvelcam <danvelcam@alum.us.es>
2024-11-11 14:57:58 +0100

    feat: test suite for notepads entity

--- core/selenium/common.py
+++ core/selenium/common.py
@@ -1,14 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-
+import os
 
 def initialize_driver():
     # Initializes the browser options
     options = webdriver.ChromeOptions()
 
     # Initialise the browser using WebDriver Manager
-    service = Service(ChromeDriverManager().install())
+    driver_path = ChromeDriverManager().install()
+    chromedriver_binary = os.path.join(os.path.dirname(driver_path), 'chromedriver')
+    service = Service(chromedriver_binary)
     driver = webdriver.Chrome(service=service, options=options)
     return driver
 
